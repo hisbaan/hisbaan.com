6fcfdccdc3bddf406ab1a8192143dd89be79f1c0
hisbaan <hisbaan@gmail.com>
2021-11-20 20:03:43 -0500

    Fix JS and enable modal popups for skills

--- js/modal.js
+++ js/modal.js
@@ -1,19 +1,44 @@
 window.addEventListener("DOMContentLoaded", () => {
     const modal = document.getElementById("modal");
-    const button = document.getElementById("skill");
-    const span = document.getElementById("close");
+    const buttons = document.getElementsByClassName("skill");
+    const popups = document.getElementsByClassName("skill-popup");
+    const closeButtons = document.getElementsByClassName("close-skill-popup");
 
-    button.onclick = function () {
-        modal.style.display = "grid";
+    // Popuplate the key-value pair linking the skill of the popup to the popup.
+    let popupsIndex = {};
+    for (i = 0; i < popups.length; i++) {
+        popupsIndex[popups[i].dataset.skill] = popups[i]
     }
 
-    span.onclick = function () {
-        modal.style.display = "none";
+    // Track the curren popup to eliminate the need for iterating through
+    //  entire list of popups
+    let openPopup = popups[0]
+
+    for(i = 0; i < buttons.length; i++) {
+        let skill = buttons[i].dataset.skill
+        // Set onclick for all the buttons
+        buttons[i].onclick = function () {
+            // Toggle the popup background
+            modal.style.display = "grid";
+            popupsIndex[skill].style.display = "block";
+            // Update openPopup
+            openPopup = popupsIndex[skill]
+        }
     }
 
+    // Set onclick for the popup background to close the popup.
     window.onclick = function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
+            openPopup.style.display = "none";
+        }
+    }
+
+    // Set onclick for the popup background to close the popup.
+    for(i = 0; i < closeButtons.length; i++) {
+        closeButtons[i].onclick = function () {
+            modal.style.display = "none";
+            openPopup.style.display = "none";
         }
     }
 })
