8924bc1988ac55971f233d408fb05bd6aa1cbac9
hisbaan <hisbaan@gmail.com>
2024-05-16 14:37:08 -0400

    Add hover effect to top-nav links

--- src/components/top-nav.tsx
+++ src/components/top-nav.tsx
@@ -4,10 +4,19 @@ export default function TopNav() {
   return (
     <>
       <div className="flex h-20 w-full items-center justify-between text-neutral-500">
-        <Link href="/">hisbaan noorani</Link>
+        <Link className="transition-colors hover:text-neutral-400" href="/">
+          hisbaan noorani
+        </Link>
         <div className="flex gap-5">
-          <Link href="/">home</Link>
-          <Link href="/articles">blog</Link>
+          <Link className="transition-colors hover:text-neutral-400" href="/">
+            home
+          </Link>
+          <Link
+            className="transition-colors hover:text-neutral-400"
+            href="/articles"
+          >
+            blog
+          </Link>
         </div>
       </div>
     </>
